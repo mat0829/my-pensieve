0594f1788bfcb14cb7f1743b3abe57c8a5f21fdf
mat_anderson <matandersonlv@hotmail.com>
2019-11-05 09:39:44 -0800

    added relationships in Memory Model

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,2 +1,7 @@
 class Memory < ApplicationRecord
+    belongs_to :user
+    has_many :memory_emotions
+    has_many :emotions, through: :memory_emotions
+    has_many :memory_players
+    has_many :players, through: :memory_players
 end
