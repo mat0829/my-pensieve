9e85de010d24e6f54b0978032f82796c03a1f76d
mat_anderson <matandersonlv@hotmail.com>
2019-11-05 12:38:40 -0800

    added basic actions to users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,41 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def show
+    
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params(
+    params.require(:user).permit(
+      :username,
+      :email,
+      :password
+    )
+  end
+  
 end
