57bcb555cc681b5f38f9678371aa0602417a5c32
mat_anderson <matandersonlv@hotmail.com>
2019-11-05 11:46:57 -0800

    added logged_in?, current_user, and redirect_if_not_logged_in methods to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    before_action :current_user
+    before_action :require_logged_in, except: [:new, :create, :home]
+
+  def logged_in?
+    !!current_user
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def redirect_if_not_logged_in
+    redirect to root_path unless logged_in?
+  end
+
+  helper_method :current_user
 end
