a98f0091b3197421d9c721e67e8c857f6b62bd5e
mat_anderson <matandersonlv@hotmail.com>
2019-11-05 12:52:09 -0800

    filled in basic actions infor for users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,23 +6,30 @@ class UsersController < ApplicationController
   end
 
   def show
-    
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+    else
+      render 'new'
+    end
   end
 
   def edit
-
   end
 
   def update
-
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    
   end
 
   private
