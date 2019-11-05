53db9174692dcc069592bb1120dda9966d2e8f61
mat_anderson <matandersonlv@hotmail.com>
2019-11-05 09:37:00 -0800

    added has_secure_password, validations, and relationships in User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :username, :email, :password, presence: true
+    validates :email, :username, uniqueness: true
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+    has_many :memories
+    has_many :emotions, through: :memories
+    has_many :players, through: :memories
 end
