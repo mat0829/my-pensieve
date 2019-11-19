f6cec94e59e13a3fe6867149314ddaee044d608e
mat_anderson <matandersonlv@hotmail.com>
2019-11-18 17:14:03 -0800

    built basic actions for emotions_controller.rb

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -1,2 +1,38 @@
 class EmotionsController < ApplicationController
+    before_action :set_emotion, only: [:show, :edit, :update, :delete, :destroy]
+  
+  def index
+    @emotions = current_user.emotions
+
+    render 'index'
+  end
+
+  def new
+    @emotions = Emotions.new
+  end
+
+  def show
+    render 'show'
+  end
+
+  def edit
+    render 'edit'
+  end
+
+  def update
+    @emotion.update(emotion_params)
+    redirect_to @emotion
+  end
+
+  private
+
+  def set_emotion
+    @emotion = Emotion.find(params[:id])
+  end
+
+  def emotion_params
+    params.require(:emotion).permit(
+      :name
+    )
+  end
 end
