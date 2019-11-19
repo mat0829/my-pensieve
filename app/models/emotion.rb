5979ef79b060ee42ab7136667314a232e1d6775f
mat_anderson <matandersonlv@hotmail.com>
2019-11-19 11:41:31 -0800

    added methods emotion_name_capitalized to Emotion model, player_name_capitalized_and_split to Player Model, and added collection_boxes to views/memories/new.html.erb

--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -2,4 +2,8 @@ class Emotion < ApplicationRecord
     has_many :memory_emotions
     has_many :memories, through: :memory_emotions
     has_many :users, through: :memories
+
+    def emotion_name_capitalized
+      name.capitalize
+    end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,8 @@ class Player < ApplicationRecord
     has_many :memory_players
     has_many :memories, through: :memory_players
     has_many :users, through: :memories
+
+    def player_name_capitalized_and_split
+      name.split.map(&:capitalize).join(' ')
+    end
 end
