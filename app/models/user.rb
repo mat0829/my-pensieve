6f7b1ca594864d2f8295e74d26f1cb5a6287339c
mat_anderson <matandersonlv@hotmail.com>
2019-11-22 15:46:05 -0800

    uninstalled turbolinks to fix errors with confirm popup not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    #has_secure_password
+    has_secure_password
     #validates :username, :email, :password, presence: true
     #validates :email, :username, uniqueness: true
     #validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
