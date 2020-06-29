ed8fbad124d824365d111cbf93a9ffb72a1591a9
mat_anderson <matandersonlv@hotmail.com>
2020-06-29 11:49:10 -0700

    added in pg database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_11_05_004531) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "emotions", force: :cascade do |t|
     t.string "name"
     t.integer "emotions_count"
